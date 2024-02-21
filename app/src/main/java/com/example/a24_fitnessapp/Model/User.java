87f22d219b8e0a788270959628d376a2b4f693a8
JulesBernardBdeB <1265963@bdeb.qc.ca>
2024-02-21 09:03:15 -0500

    Commit initial

---
+++ app/src/main/java/com/example/a24_fitnessapp/Model/User.java
@@ -0,0 +1,19 @@
+package com.example.a24_fitnessapp.Model;
+
+import java.sql.Array;
+
+public class User {
+
+    private String name;
+    private String lastName;
+    private String gender;
+    private  int age;
+    private double weight;
+    private double height;
+    private String goal[];
+    private String dietRestrictions[];
+    private String dietPreference;
+    private int dailyCalorie;
+
+
+}
