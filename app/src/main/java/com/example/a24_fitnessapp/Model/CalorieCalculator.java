ece7ba79a40a1664c89e76d8436b4735a9e754aa
JulesBernardBdeB <1265963@bdeb.qc.ca>
2024-02-27 12:02:51 -0500

    Ajout HashMap METvalues

--- app/src/main/java/com/example/a24_fitnessapp/Model/CalorieCalculator.java
+++ app/src/main/java/com/example/a24_fitnessapp/Model/CalorieCalculator.java
@@ -1,10 +1,11 @@
 package com.example.a24_fitnessapp.Model;
 
+import java.util.HashMap;
 import java.util.Objects;
 
 public class CalorieCalculator {
 
-    public double CalculateBMR(User user){
+    public double CalculateBMR(NutritionProfile user){
         double BMR = 0;
         if (Objects.equals(user.getGender(), "Men")){
             BMR = 10 * user.getWeight() + 6.25 * user.getHeight() - 5 * user.getAge() + 5;
@@ -12,5 +13,25 @@ public class CalorieCalculator {
             BMR = 10 * user.getWeight() + 6.25 * user.getHeight() - 5 * user.getAge() - 161;
         }
         return BMR;
+    }
+
+    public HashMap<String, Integer> METvalues(){
+        HashMap<String, Integer> MET = new HashMap<>();
+        MET.put("Swimming: moderate", 20);
+        MET.put("Swimming: vigorous", 33);
+        MET.put("Running: slow", 26);
+        MET.put("Running: moderate", 34);
+        MET.put("Running: fast", 41);
+        MET.put("Running: very fast", 53);
+        MET.put("Cycling: slow", 26);
+        MET.put("Cycling: moderate", 33);
+        MET.put("Cycling: fast", 40);
+        MET.put("Cycling: very fast", 55);
+        MET.put("Aerobics, Step: high impact", 33);
+        MET.put("Aerobics, Step: low impact", 23);
+        MET.put("Aerobics: high impact", 23);
+        MET.put("Aerobics: low impact", 18);
+        MET.put("Aerobics: water", 13);
+        MET.put("Calisthenics: moderate", 15);
     }
 }
